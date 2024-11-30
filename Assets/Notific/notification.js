f4e894c55ada6dc0ff281519860524a2354ea58d
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-11-30 20:26:02 +0500

    Update

--- Assets/Notific/notification.js
+++ Assets/Notific/notification.js
@@ -5,3 +5,23 @@ function opengitrepo() {
 function hideNotice() {
     document.getElementById("Notication").style.display = "none";
 }
+function setHideNoticeTimestamp() {
+    const now = new Date().getTime();
+    localStorage.setItem('hideNoticeTimestamp', now);
+}
+
+function shouldShowNotice() {
+    const hideNoticeTimestamp = localStorage.getItem('hideNoticeTimestamp');
+    if (!hideNoticeTimestamp) {
+        return true;
+    }
+    const now = new Date().getTime();
+    const eighteenHoursInMillis = 18 * 60 * 60 * 1000;
+    return now - hideNoticeTimestamp > eighteenHoursInMillis;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (shouldShowNotice()) {
+        document.getElementById("Notication").style.display = "block";
+    }
+}); 
