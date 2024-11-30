1feda4dc0a032eed8d6372aef6af343ff8d95480
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-11-30 20:29:58 +0500

    Updte

--- Assets/Notific/notification.js
+++ Assets/Notific/notification.js
@@ -4,24 +4,19 @@ function opengitrepo() {
 
 function hideNotice() {
     document.getElementById("Notication").style.display = "none";
-}
-function setHideNoticeTimestamp() {
-    const now = new Date().getTime();
-    localStorage.setItem('hideNoticeTimestamp', now);
-}
-
-function shouldShowNotice() {
-    const hideNoticeTimestamp = localStorage.getItem('hideNoticeTimestamp');
-    if (!hideNoticeTimestamp) {
-        return true;
-    }
-    const now = new Date().getTime();
-    const eighteenHoursInMillis = 18 * 60 * 60 * 1000;
-    return now - hideNoticeTimestamp > eighteenHoursInMillis;
+    var now = new Date();
+    var time = now.getTime();
+    var expireTime = time + 18 * 3600000; // 18 hours in milliseconds
+    now.setTime(expireTime);
+    document.cookie = "hideNotice=true;expires=" + now.toUTCString() + ";path=/";
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (shouldShowNotice()) {
-        document.getElementById("Notication").style.display = "block";
+window.onload = function() {
+    var cookies = document.cookie.split("; ");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].split("=");
+        if (cookie[0] === "hideNotice" && cookie[1] === "true") {
+            document.getElementById("Notication").style.display = "none";
+        }
     }
-}); 
+};
