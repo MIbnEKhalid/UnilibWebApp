bb1e95324e2cb57eeab145e89a5399999a6e697f
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-12-14 11:19:01 +0500

    Update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-import axios from "axios";
+import fs from "fs/promises"; // Use the promisified version of fs
 import path from "path";
 import { fileURLToPath } from "url";
-
 const app = express();
 // const token = process.env.GITHUB_TOKEN;
 
+const filePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "public/Assets/assigmentsNquiz.json"
+);
+
 // Prevent HTTP cache
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   res.setHeader("Pragma", "no-cache");
   res.setHeader("Expires", "0");
@@ -20,9 +24,17 @@ const __dirname = path.dirname(__filename);
 app.use("/history", express.static(path.join(__dirname, "public/history")));
 app.use("/add", express.static(path.join(__dirname, "public/add/")));
 app.use("/", express.static(path.join(__dirname, "public/")));
- 
- 
- 
+
+// Route to read file content
+app.get("/read-file", async (_, res) => {
+  try {
+    const data = await fs.readFile(filePath, "utf-8");
+    res.status(200).json({ data: JSON.parse(data) });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to read the file." });
+  }
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
