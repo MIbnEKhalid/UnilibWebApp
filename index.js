ea2fa896530ffbc6d6e7f6b8ecb71444e69afa6c
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-12-14 20:42:45 +0500

    Update Firebase service account configuration with hardcoded values

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ import crypto from "crypto";
 import cors from "cors";
 import admin from "firebase-admin";
 
-const serviceAccount = {
+const serviceAccount =  {
   "type": "service_account",
-  "project_id": process.env.project_id,
-  "private_key_id": process.env.private_key_id,
+  "project_id": "unilib-98598",
+  "private_key_id": "0b76eacb46de4895ac315896d395d6813e1dd643",
   "private_key": process.env.private_key,
-  "client_email": process.env.client_email,
-  "client_id": process.env.client_id,
+  "client_email": "firebase-adminsdk-zu9tu@unilib-98598.iam.gserviceaccount.com",
+  "client_id": "116911042752294791781",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zu9tu%40unilib-98598.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 };
 console.log(serviceAccount);
