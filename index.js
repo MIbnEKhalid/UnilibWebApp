1f00959e528d877f3f658c6e1999f9751e7cc3d9
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-12-14 20:31:08 +0500

    Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const serviceAccount = {
   "private_key_id": process.env.private_key_id,
   "private_key": process.env.private_key,
   "client_email": process.env.dburl,
-  "client_id": process.env.dburl,
+  "client_id": process.env.client_id,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
